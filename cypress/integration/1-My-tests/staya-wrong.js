23111a370dd23dc6f45d7dd3792572a31d2ff151
Anna Tsvetkova <62999844+snegina@users.noreply.github.com>
2022-07-18 13:17:20 +0300

    Update staya-wrong.js

--- cypress/integration/1-My-tests/staya-wrong.js
+++ cypress/integration/1-My-tests/staya-wrong.js
@@ -3,8 +3,8 @@ describe('Тестирование staya', function () {
     it('Авторизация с неверным паролем и поиск совпадения с названием Невозможно войти', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('snegina@yandex.ru');
-        cy.get('.auth-form > form > [type="password"]').type('123456');
+        cy.get('.auth-form > form > [type="email"]').type('email');
+        cy.get('.auth-form > form > [type="password"]').type('wrong_password');
         cy.get('.auth-form__submit > .s-button__content').click();
         cy.contains('Невозможно войти')
 
