508c38a65117d157dde3ad8218b588bcfa13fc3d
Anna Tsvetkova <snegina@yandex.ru>
2022-07-19 16:38:49 +0300

    Update staya.js

--- cypress/integration/1-My-tests/staya.js
+++ cypress/integration/1-My-tests/staya.js
@@ -4,8 +4,8 @@ describe('Тестирование staya', function () {
     it('Авторизовываюсь и ищу совпадение с названием Мои заказы', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('email');
-        cy.get('.auth-form > form > [type="password"]').type('password');
+        cy.get('.auth-form > form > [type="email"]').type('your_email');
+        cy.get('.auth-form > form > [type="password"]').type('your_password');
         cy.get('.auth-form__submit > .s-button__content').click();
         cy.contains('Мои заказы')
 
